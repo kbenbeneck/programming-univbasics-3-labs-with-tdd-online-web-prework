fb7484d3ecb7e5227da24ccb3202813da77e1509
Karl Benbeneck <kbenbeneck@gmail.com>
2020-02-03 03:49:57 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -6,3 +6,7 @@ second_number = 100
 sum = first_number + second_number
 
 difference = first_number - second_number
+
+product = first_number * second_number
+
+quotient = first_number / second_number
